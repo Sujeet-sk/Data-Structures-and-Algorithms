fa0fa184155cc5461904d0d324ce1d86a9b2eea1
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-13 17:11:10 +0530

    Added Solution - GfG to GitHub

---
+++ GeeksforGeeks/Java/Difficulty: Basic/Preorder Traversal/preorder-traversal.java
@@ -0,0 +1,28 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        Stack<Node>st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            Node top=st.pop();
+            ans.add(top.data);
+            if(top.right!=null) st.push(top.right);
+            if(top.left!=null) st.push(top.left);
+        }
+        return ans;
+    }
+}
