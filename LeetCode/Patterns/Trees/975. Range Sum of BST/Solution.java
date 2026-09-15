9bde647e85b693946cecc5a751d69c7b56dd04a3
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-15 12:02:57 +0530

    Add Easy Java Solution for 975. Range Sum of BST from LeetCode

---
+++ LeetCode/Patterns/Trees/975. Range Sum of BST/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        TreeNode temp=root;
+        if(temp==null) return 0;
+        if(temp.val<low) return rangeSumBST(temp.right,low,high);
+        else if(temp.val>high) return rangeSumBST(temp.left,low,high);
+        else return temp.val + rangeSumBST(temp.left,low,high) + rangeSumBST(temp.right,low,high);
+    }
+}
