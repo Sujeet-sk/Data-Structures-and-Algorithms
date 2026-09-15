363ed7e5ef88a66ccfad87a2d1dd9d751f180cda
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-15 14:07:19 +0530

    Add Easy Java Solution for 94. Binary Tree Inorder Traversal from LeetCode

--- LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/README.md
+++ LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/README.md
@@ -9,7 +9,7 @@ Stack, Tree, Depth-First Search, Binary Tree
 
 ### 🚀 Performance
 - **Runtime:** 0 ms
-- **Memory:** 43.1 MB
+- **Memory:** 43 MB
 
 ---
 
--- LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/Solution.java
+++ LeetCode/Java/Easy/94. Binary Tree Inorder Traversal/Solution.java
@@ -14,15 +14,28 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,ArrayList<Integer>ans){
-        if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer>ans=new ArrayList<>();
-        inorder(root,ans);
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr) pred=pred.right;
+                if(pred.right==null){
+                    pred.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    pred.right=null;
+                    ans.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+            else{
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+        }
         return ans;
     }
 }
