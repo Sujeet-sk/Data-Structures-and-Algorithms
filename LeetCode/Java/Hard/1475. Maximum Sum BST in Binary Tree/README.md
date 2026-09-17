ab32f247fc91bc2cef768956604be9fcccf88d69
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-17 12:52:16 +0530

    Add Hard Java Solution for 1475. Maximum Sum BST in Binary Tree from LeetCode

--- LeetCode/Java/Hard/1475. Maximum Sum BST in Binary Tree/README.md
+++ LeetCode/Java/Hard/1475. Maximum Sum BST in Binary Tree/README.md
@@ -8,8 +8,8 @@
 Dynamic Programming, Tree, Depth-First Search, Binary Search Tree, Binary Tree, DP on Trees
 
 ### 🚀 Performance
-- **Runtime:** 8 ms
-- **Memory:** 63.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Hard/1475. Maximum Sum BST in Binary Tree/Solution.java
+++ LeetCode/Java/Hard/1475. Maximum Sum BST in Binary Tree/Solution.java
@@ -16,33 +16,32 @@
 class Quad{
     int max;
     int min;
-    boolean isBST;
     int sum;
+    boolean isBST;
 
-    Quad(int max, int min, boolean isBST, int sum){
+    Quad(int max,int min,int sum,boolean isBST){
         this.max=max;
         this.min=min;
-        this.isBST=isBST;
         this.sum=sum;
+        this.isBST=isBST;
     }
 }
 class Solution {
-    static int maxSum;
-    public int maxSumBST(TreeNode root) {
-        maxSum=0;
-        maxMin(root);
-        return maxSum;
-    }
-
-    Quad maxMin(TreeNode root){
-        if(root==null) return new Quad(Integer.MIN_VALUE,Integer.MAX_VALUE,true,0);
-        Quad lft=maxMin(root.left);
-        Quad rgt=maxMin(root.right);
+    int maxSum;
+    public Quad helper(TreeNode root){
+        if(root==null) return new Quad(Integer.MIN_VALUE,Integer.MAX_VALUE,0,true);
+        Quad lft=helper(root.left);
+        Quad rgt=helper(root.right);
         int max=Math.max(root.val,Math.max(lft.max,rgt.max));
         int min=Math.min(root.val,Math.min(lft.min,rgt.min));
-        boolean isBST=lft.isBST && rgt.isBST && (lft.max<root.val) && (rgt.min>root.val);
         int sum=root.val+lft.sum+rgt.sum;
-        if(isBST) maxSum=Math.max(sum,maxSum);
-        return new Quad(max,min,isBST,sum);
+        boolean isBST=lft.isBST && rgt.isBST && (lft.max<root.val) && (rgt.min>root.val);
+        if(isBST) maxSum=Math.max(maxSum,sum);
+        return new Quad(max,min,sum,isBST);
+    }
+    public int maxSumBST(TreeNode root) {
+        maxSum=0;
+        helper(root);
+        return maxSum;
     }
 }
