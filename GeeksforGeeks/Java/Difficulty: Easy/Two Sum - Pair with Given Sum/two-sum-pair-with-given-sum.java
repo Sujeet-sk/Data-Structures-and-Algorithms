95ee946df34f1d3d5dfe7733aacf24b8012e4b4c
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-18 12:03:15 +0530

    Added Solution - GfG to GitHub

---
+++ GeeksforGeeks/Java/Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int ele:arr){
+            int rem=target-ele;
+            if(set.contains(rem)) return true;
+            else set.add(ele);
+        }
+        return false;
+    }
+}
