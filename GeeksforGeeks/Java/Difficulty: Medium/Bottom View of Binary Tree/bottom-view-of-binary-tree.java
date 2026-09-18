5b8993049627ea56c8476e2d1701ad0b6b44290a
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-18 15:01:56 +0530

    Added Solution - GfG to GitHub

---
+++ GeeksforGeeks/Java/Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -0,0 +1,41 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+
+    }
+}
+*/
+class Pair{
+    Node node;
+    int hd;
+
+    Pair(Node node,int hd){
+        this.node=node;
+        this.hd=hd;
+    }
+}
+class Solution {
+    public ArrayList<Integer> bottomView(Node root) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+        Queue<Pair>q=new LinkedList<>();
+        q.add(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair front=q.remove();
+            int hd=front.hd;
+            map.put(hd,front.node.data);
+            if(front.node.left!=null) q.add(new Pair(front.node.left,hd-1));
+            if(front.node.right!=null) q.add(new Pair(front.node.right,hd+1));
+        }
+        for(int ele:map.keySet()) ans.add(map.get(ele));
+        
+        return ans;
+    }
+}
