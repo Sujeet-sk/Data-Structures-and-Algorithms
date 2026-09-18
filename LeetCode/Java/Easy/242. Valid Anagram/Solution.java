c20c5d58ffe79a8cffd1bc472d3bff4fae70575d
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-18 13:17:30 +0530

    Add Easy Java Solution for 242. Valid Anagram from LeetCode

--- LeetCode/Java/Easy/242. Valid Anagram/Solution.java
+++ LeetCode/Java/Easy/242. Valid Anagram/Solution.java
@@ -1,20 +1,23 @@
-class Solution {
-    public boolean isAnagram(String s1, String s2) {
-        if(s1.length()!=s2.length()) return false;
-        int[] freq=new int[26];
-        for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            int idx=ch-97;
-            freq[idx]++;
+    class Solution {
+        public boolean isAnagram(String s1, String s2) {
+            if(s1.length()!=s2.length()) return false;
+            int[] freq=new int[26];
+
+            for(int i=0;i<s1.length();i++){
+                char ch=s1.charAt(i);
+                int idx=ch-97;
+                freq[idx]++;
+            }
+
+            for(int i=0;i<s2.length();i++){
+                char ch=s2.charAt(i);
+                int idx=ch-97;
+                freq[idx]--;
+            }
+            
+            for(int i=0;i<freq.length;i++){
+                if(freq[i]!=0) return false;
+            }
+            return true;
         }
-        for(int i=0;i<s2.length();i++){
-            char ch=s2.charAt(i);
-            int idx=ch-97;
-            freq[idx]--;
-        }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0) return false;
-        }
-        return true;
     }
-}
