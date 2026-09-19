e9378dcf8924672c29479ee6e8a795525ae0b10e
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-19 11:18:15 +0530

    Add Medium Java Solution for 912. Sort an Array from LeetCode

--- LeetCode/Patterns/Heap & Priority Queue/912. Sort an Array/Solution.java
+++ LeetCode/Patterns/Heap & Priority Queue/912. Sort an Array/Solution.java
@@ -1,11 +1,31 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>();
-        int[] ans=new int[nums.length];
-        int idx=0;
-        for(int i=0;i<nums.length;i++) pq.add(nums[i]);
-        while(pq.size()>0) ans[idx++]=pq.remove();
+    public void quickSort(int[] nums,int low,int high){
+        if(low<high){
+            int pivot_idx=partition(nums,low,high);
+            quickSort(nums,low,pivot_idx-1);
+            quickSort(nums,pivot_idx+1,high);
+        }
 
-        return ans;
+    }
+    public int partition(int[] nums,int low,int high){
+        int pivot=nums[high];
+        int idx=low;
+        for(int i=low;i<high;i++){
+            if(nums[i]<pivot){
+                int temp=nums[i];
+                nums[i]=nums[idx];
+                nums[idx++]=temp;
+            }
+        }
+        int temp=nums[idx];
+        nums[idx]=nums[high];
+        nums[high]=temp; 
+
+        return idx;
+    }
+    public int[] sortArray(int[] nums) {
+        int n=nums.length;
+        quickSort(nums,0,n-1);
+        return nums;
     }
 }
