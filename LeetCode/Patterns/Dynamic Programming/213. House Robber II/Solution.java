a44ce6aef1315f8e75d069d9782d55d4cf360ff6
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-25 17:38:50 +0530

    Add Medium Java Solution for 213. House Robber II from LeetCode

--- LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
@@ -1,17 +1,22 @@
 class Solution {
-    public int maxAmount(int i,int j,int[] nums,int[] dp){
+    static int[] dp;
+    public int loot(int i,int j,int[] nums){
         if(i>j) return 0;
-        if(dp[i]!=-1 && dp[i]<nums[i]) return dp[i];
-        int pick=nums[i]+maxAmount(i+2,j,nums,dp);
-        int skip=maxAmount(i+1,j,nums,dp);
-        return dp[i]=Math.max(pick,skip);
+        if(dp[i]!=-1) return dp[i];
+        int pick=nums[i]+loot(i+2,j,nums);
+        int skip=loot(i+1,j,nums);
+        int ans=Math.max(pick,skip);
+        dp[i]=ans;
+        return ans;
     }
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int[] dp=new int[nums.length];
         int n=nums.length;
+        if(n==1) return nums[0];
+        dp=new int[n];
         Arrays.fill(dp,-1);
-        return Math.max(maxAmount(0,n-2,nums,dp),maxAmount(1,n-1,nums,dp));
-
+        int first=loot(0,n-2,nums);
+        Arrays.fill(dp,-1);
+        int second=loot(1,n-1,nums);
+        return Math.max(first,second);
     }
 }
