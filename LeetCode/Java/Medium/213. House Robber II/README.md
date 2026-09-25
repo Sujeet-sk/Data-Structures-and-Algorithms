c49cafc1a9e245d7cd2d0072535cbac4d1e1d2a9
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-25 17:36:42 +0530

    Add Medium Java Solution for 213. House Robber II from LeetCode

--- LeetCode/Java/Medium/213. House Robber II/README.md
+++ LeetCode/Java/Medium/213. House Robber II/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 42.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.java
@@ -1,22 +1,16 @@
 class Solution {
-    static int[] dp;
-    public int loot(int i,int j,int[] nums){
+    public int maxAmount(int i,int j,int[] nums,int[] dp){
         if(i>j) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick=nums[i]+loot(i+2,j,nums);
-        int skip=loot(i+1,j,nums);
-        int ans=Math.max(pick,skip);
-        dp[i]=ans;
-        return ans;
+        if(dp[i]!=-1 && dp[i]<nums[i]) return dp[i];
+        int pick=nums[i]+maxAmount(i+2,j,nums,dp);
+        int skip=maxAmount(i+1,j,nums,dp);
+        return dp[i]=Math.max(pick,skip);
     }
     public int rob(int[] nums) {
+        int[] dp=new int[nums.length];
         int n=nums.length;
-        if(n==1) return nums[0];
-        dp=new int[n];
         Arrays.fill(dp,-1);
-        int first=loot(0,n-2,nums);
-        Arrays.fill(dp,-1);
-        int second=loot(1,n-1,nums);
-        return Math.max(first,second);
+        return Math.max(maxAmount(0,n-2,nums,dp),maxAmount(1,n-1,nums,dp));
+
     }
 }
